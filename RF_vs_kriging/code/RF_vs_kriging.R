0802b869b982a80e622aa1399bdb05be4eda560b
thengl <tom.hengl@gmail.com>
2017-12-06 17:31:40 +0100

    example with copula

--- RF_vs_kriging/code/RF_vs_kriging.R
+++ RF_vs_kriging/code/RF_vs_kriging.R
@@ -301,6 +301,57 @@ plt.UK = xyplot(cv.UK[[1]]$Observed~cv.UK[[1]]$Predicted, asp=1, par.settings=li
 grid.arrange(plt.UK, plt.RF2, ncol=2)
 dev.off()
 
+## Intamap example ----
+library(intamap)
+library(gstat)
+data(sic2004)
+coordinates(sic.val) <- ~x+y
+sic.val$value <- sic.val$dayx
+#writeOGR(sic.val, "results/sic2004/sic.val.shp", "sic.val", "ESRI Shapefile")
+coordinates(sic.test) <- ~x+y
+pred.sic2004 <- interpolate(sic.val, sic.test, maximumTime = 90)
+#R 2017-12-06 15:00:58 interpolating 200 observations, 808 prediction locations
+#[1] "estimated time for copula 71.2993216100125"
+spplot(pred.sic2004$predictions[1])
+#plot(sic.test$dayx~pred.sic2004$predictions$mean, asp=1)
+sd(sic.test$dayx-pred.sic2004$predictions$mean)
+## 12.4
+
+## RFsp
+bbox=sic.val@bbox
+bbox[,"min"]=bbox[,"min"]-4000
+bbox[,"max"]=bbox[,"max"]+4000
+de2km = plotKML::vect2rast(sic.val, cell.size=2000, bbox=bbox)
+de2km$mask = 1
+de2km = as(de2km["mask"], "SpatialPixelsDataFrame")
+plot(de2km); points(sic.val)
+de.dist0 <- buffer.dist(sic.val["value"], de2km, as.factor(1:nrow(sic.val@data)))
+ov.de = over(sic.val["value"], de.dist0)
+de.dn0 <- paste(names(de.dist0), collapse="+")
+de.fm1 <- as.formula(paste("value ~ ", de.dn0))
+de.rm = do.call(cbind, list(sic.val@data["value"], ov.de))
+## fine-tune RF:
+#rt.gamma <- makeRegrTask(data = de.rm[complete.cases(de.rm[,all.vars(de.fm1)]),], target = "value")
+#estimateTimeTuneRF(rt.gamma)
+## Too time-consuming
+#t.gamma <- tuneRF(rt.gamma, num.trees = 100, build.final.model = FALSE)
+#t.gamma
+#pars.gamma = list(mtry= t.gamma$recommended.pars$mtry, min.node.size=t.gamma$recommended.pars$min.node.size, sample.fraction=t.gamma$recommended.pars$sample.fraction, num.trees=100)
+m1.gamma <- quantregRanger(de.fm1, de.rm[complete.cases(de.rm),], params.ranger = list(importance = "impurity", mtry=180, num.trees=200))
+m1.gamma
+## R squared (OOB): 0.5745
+gamma.rfd1 <- predict(m1.gamma, de.dist0@data, quantiles)
+de2km$gamma_rfd1 = gamma.rfd1[,2]
+de2km$gamma_rfd1_var = (gamma.rfd1[,3]-gamma.rfd1[,1])/2
+plot(de2km["gamma_rfd1"])
+points(sic.val, pch="+")
+#pred2km.sic2004 <- interpolate(sic.val, de2km, methodName = "automap")
+#de2km$gamma_ok = pred2km.sic2004$predictions$var1.pred
+#plot(de2km["gamma_ok"])
+#points(sic.val, pch="+")
+ov.test <- over(sic.test, de2km["gamma_rfd1"])
+plot(sic.test$dayx~ov.test$gamma_rfd1, asp=1)
+sd(sic.test$dayx-ov.test$gamma_rfd1, na.rm=TRUE)
 
 ## Ebergotzen binomial variable ----
 library(plotKML)
@@ -513,71 +564,6 @@ plot(raster(usa5km["MG_ICP40_rf"]), col=leg, main="Mg (wt%)", axes=FALSE, box=FA
 points(geochem[!is.na(geochem$MG_ICP40),], pch="+", cex=.5)
 dev.off()
 
-library(RCurl)
-library(rgdal)
-nl.rd <- getURL("http://spatialreference.org/ref/sr-org/6781/proj4/")
-## Geul data set ----
-geul <- read.table("data/geul/geul.dat", header = TRUE, as.is = TRUE)
-geul$pb = as.numeric(geul$pb)
-geul = geul[!is.na(geul$pb),]
-coordinates(geul) <- ~x+y
-proj4string(geul) <- CRS(nl.rd) 
-grd25 <- readGDAL("data/geul/dem25.txt")
-grd25 <- as(grd25, "SpatialPixelsDataFrame")
-proj4string(grd25) = proj4string(geul) 
-
-## Pb predicted using OK
-library(geoR)
-pb.geo <- as.geodata(geul["pb"])
-pb.vgm <- likfit(pb.geo, lambda=0, messages=FALSE, ini=c(var(log1p(pb.geo$data)),500), cov.model="exponential")
-locs2 = grd25@coords
-pb.ok <- krige.conv(pb.geo, locations=locs2, krige=krige.control(obj.model=pb.vgm))
-grd25$pb_ok = pb.ok$predict
-## Pb predicted using RF only
-ov.geul = over(geul["pb"], grd25)
-summary(ov.geul$band1)
-geul.s = geul[!is.na(ov.geul$band1),"pb"]
-grid.dist1 <- buffer.dist(geul.s, grd25[1], as.factor(1:nrow(geul.s)))
-dn1 <- paste(names(grid.dist1), collapse="+")
-fm1 <- as.formula(paste("pb ~", dn1))
-m1 <- fit.gstatModel(geul.s, fm1, grid.dist1, method="ranger", rvgm=NULL)
-rk.m1 <- predict(m1, grid.dist1)
-
-## Plot predictions next to each other:
-grd25$pb_ok = ifelse(grd25$pb_ok<expm1(4.2), expm1(4.2), grd25$pb_ok)
-png(file = "results/geul/Fig_comparison_OK_RF_Pb_Geul.png", res = 150, width = 1750, height = 1200)
-par(mfrow=c(1,2), oma=c(0,0,0,0))
-plot(log1p(raster(grd25["pb_ok"])), col=leg, zlim=c(4.2,6.6), main="geoR (krige.conv)")
-points(geul.s, pch="+")
-plot(log1p(raster(rk.m1@predicted[2])), col=leg, zlim=c(4.2,6.6), main="Random Forest")
-points(geul.s, pch="+")
-dev.off()
-
-## RF with both buffer dist and covariates ----
-grd25$swi <- readGDAL("data/geul/swi.sdat")$band1[grd25@grid.index]
-grd25$dis <- readGDAL("data/geul/riverdist.txt")$band1[grd25@grid.index]
-plot(stack(grd25))
-grd25T <- grd25[c("band1","swi","dis")]
-grd25T@data <- cbind(grd25T@data, grid.dist1@data)
-## Run principal component analysis:
-grd25.spc <- spc(grd25T, as.formula(paste("~", paste(names(grd25T), collapse = "+"))))
-plot(stack(grd25.spc@predicted[1:6]))
-fm2 <- as.formula(paste("pb ~", paste(names(grd25.spc@predicted), collapse = "+")))
-m2 <- fit.gstatModel(geul.s, fm2, grd25.spc@predicted, method="quantregForest", rvgm=NULL)
-plot(m2)
-dev.off()
-rk.m2 <- predict(m2, grd25.spc@predicted)
-#plot(rk.m2, col=leg)
-# varImpPlot(m1@regModel)
-
-rk.m2@predicted$pb = ifelse(rk.m2@predicted$pb<expm1(4.2), expm1(4.2), rk.m2@predicted$pb)
-png(file = "results/geul/Fig_comparison_RF_covariates_Pb_Geul.png", res = 150, width = 1750, height = 1200)
-par(mfrow=c(1,2), oma=c(0,0,0,0))
-plot(log1p(raster(rk.m2@predicted[2])), col=leg, zlim=c(4.2,6.6), main="Random Forest + covs")
-points(geul.s, pch="+")
-plot(log1p(raster(rk.m1@predicted[2])), col=leg, zlim=c(4.2,6.6), main="Random Forest")
-points(geul.s, pch="+")
-dev.off()
 
 ## Spatiotemporal prediction using Random Forest ----
 ## Daily precipitation obtained from https://www.ncdc.noaa.gov/cdo-web/search
